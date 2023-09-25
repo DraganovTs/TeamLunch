650e079551ff7ee9166a432d3c263f249318b8dc
DraganovTs <cvetomir_draganov@abv.bg>
2023-09-25 20:51:39 +0300

    user-service

---
+++ user-service/src/test/java/com/homecode/user/service/UserServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.homecode.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceApplicationTests {
+    @Test
+    public void contextLoad(){
+
+    }
+}
